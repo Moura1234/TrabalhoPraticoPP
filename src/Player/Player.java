b5911ac05ff57ee7e00de4ab49647cdd627ed925
Moura1234 <8230310@estg.ipp.pt>
2025-05-13 18:41:17 +0100

    aaaa
    aa

---
+++ src/Player/Player.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Player;
+
+import java.time.LocalDate;
+import Enums.Position;
+/**
+ *
+ * @author joaom
+ */
+public class Player {
+    private String name;
+    private LocalDate birthdate;
+    private int age;
+    private String nationality;
+    private int number;
+    private int shooting;
+    private int passing;
+    private int stamina;
+    private int speed;
+    private Position position;
+    
+}
