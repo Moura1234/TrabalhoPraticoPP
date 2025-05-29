72d10fc4964abe3ca5700e35f7834912dfdfbe63
Moura1234 <8230310@estg.ipp.pt>
2025-05-29 19:30:50 +0100

    exceptions and documentation

--- src/Player/Player.java
+++ src/Player/Player.java
@@ -2,24 +2,30 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
+/**
+ * Nome: João Miguel Oliveira Moura
+ * Número: 8230310 
+ * Turma: LSIRC 1T2
+ *
+ * Nome: Rodrigo António Amorim Gonçalo Soares
+ * Número: 8230329
+ * Turma: LSIRC 1T2
+ */
 package Player;
-
 
 import com.ppstudios.footballmanager.api.contracts.player.*;
 import java.time.LocalDate;
 import Enums.Position;
 import java.io.IOException;
 import com.ppstudios.footballmanager.api.contracts.player.PreferredFoot;
-
 import java.time.LocalDate;
 import Enums.Position;
 
 /**
- *
- * @author joaom
+ * Class representing a football player with personal and technical attributes.
+ * Implements the IPlayer interface
  */
-
-public class Player implements IPlayer{
+public class Player implements IPlayer {
 
     private String name;
     private LocalDate birthDate;
@@ -36,9 +42,30 @@ public class Player implements IPlayer{
     private float height;
     private float weight;
     private PreferredFoot preferredFoot;
-    
-    public Player(String name, LocalDate birthDate, String nationality, int number, int shooting, int passing, int stamina,int defense, 
-            int speed, int reflexes, IPlayerPosition position, String photo, float height, float weight, PreferredFoot preferredFoot){
+
+    /**
+     * Constructs a Player with all attributes defined.
+     *
+     * @param name Player's name
+     * @param birthDate Player's birth date
+     * @param nationality Player's nationality
+     * @param number Player's number
+     * @param shooting Shooting skill
+     * @param passing Passing skill
+     * @param stamina Stamina level
+     * @param defense Defensive ability
+     * @param speed Speed attribute
+     * @param reflexes Reflexes attribute
+     * @param position Player's field position
+     * @param photo Path or URL to the player's photo
+     * @param height Player's Height
+     * @param weight Player's Weight
+     * @param preferredFoot Player's preferred foot (left or right)
+     */
+    public Player(String name, LocalDate birthDate, String nationality, int number,
+            int shooting, int passing, int stamina, int defense,
+            int speed, int reflexes, IPlayerPosition position,
+            String photo, float height, float weight, PreferredFoot preferredFoot) {
         this.name = name;
         this.birthDate = birthDate;
         this.nationality = nationality;
@@ -56,92 +83,186 @@ public class Player implements IPlayer{
         this.preferredFoot = preferredFoot;
     }
 
+    /**
+     * Gets the name of the player.
+     *
+     * @return Player's name
+     */
     @Override
     public String getName() {
         return this.name;
     }
 
+    /**
+     * Gets the birth date of the player.
+     *
+     * @return Player's birth date
+     */
     @Override
     public LocalDate getBirthDate() {
         return this.birthDate;
     }
-    
+
+    /**
+     * Gets the age of the player.
+     *
+     * @return Player's age (currently hardcoded)
+     */
     @Override
     public int getAge() {
-        return 3;
+        return 3; // TODO: replace with actual age calculation
     }
 
+    /**
+     * Gets the nationality of the player.
+     *
+     * @return Player's nationality
+     */
     @Override
     public String getNationality() {
         return this.nationality;
     }
-    
+
+    /**
+     * Sets the player's position.
+     *
+     * @param ipp New player position
+     * @throws IllegalArgumentException if the position is null
+     */
     @Override
-    public void setPosition(IPlayerPosition ipp){
+    public void setPosition(IPlayerPosition ipp) {
+        if (ipp == null) {
+            throw new IllegalArgumentException("Player position cannot be null.");
+        }
         this.position = ipp;
     }
-           
+
+    /**
+     * Gets the player's number.
+     *
+     * @return Jersey number
+     */
     @Override
     public int getNumber() {
         return this.number;
     }
 
+    /**
+     * Gets the shooting skill of the player.
+     *
+     * @return Shooting value
+     */
     @Override
     public int getShooting() {
         return this.shooting;
     }
 
+    /**
+     * Gets the passing skill of the player.
+     *
+     * @return Passing value
+     */
     @Override
     public int getPassing() {
         return this.passing;
     }
 
+    /**
+     * Gets the stamina of the player.
+     *
+     * @return Stamina value
+     */
     @Override
     public int getStamina() {
         return this.stamina;
     }
 
+    /**
+     * Gets the defensive ability of the player.
+     *
+     * @return Defense value
+     */
     public int getDefense() {
         return defense;
     }
 
+    /**
+     * Gets the speed of the player.
+     *
+     * @return Speed value
+     */
     @Override
     public int getSpeed() {
         return this.speed;
     }
 
-    public int getReflexes(){
+    /**
+     * Gets the reflexes of the player.
+     *
+     * @return Reflexes value
+     */
+    public int getReflexes() {
         return this.reflexes;
     }
-    
+
+    /**
+     * Gets the current position of the player.
+     *
+     * @return Player's position
+     */
     @Override
     public IPlayerPosition getPosition() {
         return this.position;
     }
 
+    /**
+     * Gets the photo path or URL of the player.
+     *
+     * @return Photo path or URL
+     */
     @Override
     public String getPhoto() {
         return this.photo;
     }
 
+    /**
+     * Gets the height of the player.
+     *
+     * @return Height in meters
+     */
     @Override
     public float getHeight() {
         return this.height;
     }
 
+    /**
+     * Gets the weight of the player.
+     *
+     * @return Weight in kilograms
+     */
     @Override
     public float getWeight() {
         return this.weight;
     }
 
+    /**
+     * Gets the preferred foot of the player.
+     *
+     * @return Preferred foot
+     */
     @Override
     public PreferredFoot getPreferredFoot() {
         return this.preferredFoot;
     }
-    
+
+    /**
+     * Exports the player data to a JSON file.
+     *
+     * @throws IOException if writing fails
+     * @throws UnsupportedOperationException if method is not yet implemented
+     */
     @Override
     public void exportToJson() throws IOException {
-        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+        throw new UnsupportedOperationException("Not supported yet.");
     }
-
 }
--- src/Team/Club.java
+++ src/Team/Club.java
@@ -3,6 +3,7 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
 package Team;
+
 import com.ppstudios.footballmanager.api.contracts.team.*;
 import com.ppstudios.footballmanager.api.contracts.player.*;
 import java.io.IOException;
@@ -11,7 +12,12 @@ import java.io.IOException;
  *
  * @author joaom
  */
-public class Club implements IClub{
+/**
+ * Class representing a football club. Implements the IClub interface and
+ * manages players and team logic.
+ */
+public class Club implements IClub {
+
     private String name;
     private IPlayer[] players;
     private String code;
@@ -24,9 +30,24 @@ public class Club implements IClub{
     private IPlayer selected;
     private int playerCount;
     private static final int MAX_PLAYERS = 18;
-    
-    public Club (String name, IPlayer[] players, String code, String country, 
-            int foundedYear, String stadiumName, String logo, String playerPosition, String targetPosition, IPlayer selected){
+
+    /**
+     * Constructs a club with the given information and initial player list.
+     *
+     * @param name Club name
+     * @param players Initial players array
+     * @param code Club code
+     * @param country Country of the club
+     * @param foundedYear Year the club was founded
+     * @param stadiumName Name of the club's stadium
+     * @param logo Path or URL to the club logo
+     * @param playerPosition Default player position label
+     * @param targetPosition Target position label
+     * @param selected Initially selected player
+     */
+    public Club(String name, IPlayer[] players, String code, String country,
+            int foundedYear, String stadiumName, String logo,
+            String playerPosition, String targetPosition, IPlayer selected) {
         this.name = name;
         this.players = players;
         this.code = code;
@@ -40,140 +61,302 @@ public class Club implements IClub{
         playerCount = 0;
     }
 
+    /**
+     * Gets the name of the club.
+     *
+     * @return Club name
+     */
     @Override
     public String getName() {
         return this.name;
     }
 
+    /**
+     * Gets the array of players in the club.
+     *
+     * @return Array of players
+     */
     @Override
     public IPlayer[] getPlayers() {
         return this.players;
     }
-    
-    public void setPlayers(IPlayer[] players) {
-    this.players = players;
-    this.playerCount = players.length;
-}
 
+    /**
+     * Sets the array of players and updates player count.
+     *
+     * @param players New array of players
+     */
+    public void setPlayers(IPlayer[] players) {
+        this.players = players;
+        this.playerCount = players.length;
+    }
+
+    /**
+     * Gets the club code.
+     *
+     * @return Club code
+     */
     @Override
     public String getCode() {
         return this.code;
     }
 
+    /**
+     * Gets the country of the club.
+     *
+     * @return Club country
+     */
     @Override
     public String getCountry() {
         return this.country;
     }
 
+    /**
+     * Gets the year the club was founded.
+     *
+     * @return Founded year
+     */
     @Override
     public int getFoundedYear() {
         return this.foundedYear;
     }
 
+    /**
+     * Gets the name of the stadium.
+     *
+     * @return Stadium name
+     */
     @Override
     public String getStadiumName() {
         return this.stadiumName;
     }
 
+    /**
+     * Gets the club logo.
+     *
+     * @return Logo path or URL
+     */
     @Override
     public String getLogo() {
         return this.logo;
     }
 
-   
+    /**
+     * Gets the player position label.
+     *
+     * @return Player position label
+     */
     public String getPlayerPosition() {
         return playerPosition;
     }
 
-   
-    public String getTargetPosition() { // n esta na interface
+    /**
+     * Gets the target position label.
+     *
+     * @return Target position label
+     */
+    public String getTargetPosition() {
         return targetPosition;
     }
-    
- 
+
+    /**
+     * Gets the number of players in the club.
+     *
+     * @return Player count
+     */
     @Override
     public int getPlayerCount() {
         return this.playerCount;
     }
-    
+
+    /**
+     * Selects a player using the given selector and position.
+     *
+     * @param ips Player selector strategy
+     * @param ipp Desired player position
+     * @return Selected player
+     * @throws IllegalArgumentException if the position is null
+     * @throws IllegalStateException if the club is empty or no player is found
+     * for the position
+     */
     @Override
     public IPlayer selectPlayer(IPlayerSelector ips, IPlayerPosition ipp) {
-    for (int i = 0; i < playerCount; i++) {
-        if (players[i].getPosition().equals(ipp)) {
-            return players[i];
+        if (ipp == null) {
+            throw new IllegalArgumentException("Position cannot be null.");
         }
-    }
-    return null;
-    }
-    
-    @Override
-    public void addPlayer(IPlayer ip){
-        
-        if (isPlayer(ip)) {
-    System.out.println("O jogador já se encontra no plantel: " + ip.getName());
-    return;
+
+        if (playerCount == 0) {
+            throw new IllegalStateException("Club has no players.");
+        }
+
+        IPlayer selected = ips.selectPlayer(this, ipp);
+
+        if (selected == null) {
+            throw new IllegalStateException("No player found for position: " + ipp.getDescription());
+        }
+
+        return selected;
     }
 
-           if (playerCount < MAX_PLAYERS) {
-            players[playerCount++] = ip;
-        } else {
-            System.out.println("Não foi possível adicionar " + ip.getName() + ": Plantel cheio.");
-    
-        } 
-    }
-    
+    /**
+     * Adds a player to the club if not already present and there is room.
+     *
+     * @param ip Player to add
+     * @throws IllegalArgumentException if the player is null or already in the
+     * club
+     * @throws IllegalStateException if the club is full
+     */
     @Override
-    public boolean isPlayer(IPlayer ip){
-        for (int i = 0; i < playerCount; i++) {
-            if( players[i] == ip) 
-                
-                return true;
+    public void addPlayer(IPlayer ip) {
+        if (ip == null) {
+            throw new IllegalArgumentException("Cannot add null player.");
         }
+
+        if (isPlayer(ip)) {
+            throw new IllegalArgumentException("Player is already in the squad: " + ip.getName());
+        }
+
+        if (playerCount >= MAX_PLAYERS) {
+            throw new IllegalStateException("Squad is full. Cannot add: " + ip.getName());
+        }
+
+        players[playerCount++] = ip;
+    }
+
+    /**
+     * Checks if a player is already part of the club.
+     *
+     * @param ip Player to check
+     * @return True if player is in the club, false otherwise
+     * @throws IllegalArgumentException if the player is null or not valid
+     */
+    @Override
+    public boolean isPlayer(IPlayer ip) {
+        if (ip == null) {
+            throw new IllegalArgumentException("Player cannot be null.");
+        }
+
+        if (ip.getName() == null || ip.getPosition() == null) {
+            throw new IllegalArgumentException("Invalid player: missing name or position.");
+        }
+
+        for (int i = 0; i < playerCount; i++) {
+            if (players[i] == ip) {
+                return true;
+            }
+        }
+
         return false;
     }
-    
+
+    /**
+     * Removes a player from the club if present.
+     *
+     * @param ip Player to remove
+     * @throws IllegalArgumentException if the player is null or not in the club
+     */
     @Override
-    public void removePlayer(IPlayer ip){
-        
+    public void removePlayer(IPlayer ip) {
+        if (ip == null) {
+            throw new IllegalArgumentException("Player cannot be null.");
+        }
+
+        boolean found = false;
+
         for (int i = 0; i < playerCount; i++) {
-        if (players[i] == ip) {
-            for (int j = i; j < playerCount - 1; j++) {
-                players[j] = players[j + 1];
+            if (players[i] == ip) {
+                // Shift players to remove the one at index i
+                for (int j = i; j < playerCount - 1; j++) {
+                    players[j] = players[j + 1];
+                }
+                players[--playerCount] = null;
+                found = true;
+                break;
             }
-            players[--playerCount] = null;
-            return;
+        }
+
+        if (!found) {
+            throw new IllegalArgumentException("Player is not in the club.");
         }
     }
-    }
-    
- 
+
+    /**
+     * Validates the club according to required structure and player
+     * composition.
+     *
+     * @return true if the club is valid
+     * @throws IllegalStateException if the club has missing information or
+     * invalid player conditions
+     */
     @Override
-    public boolean isValid(){
-        
-   return name != null && !name.isEmpty()
-        && code != null && !code.isEmpty()
-        && country != null && !country.isEmpty()
-        && foundedYear > 1800
-        && stadiumName != null && !stadiumName.isEmpty()
-        && logo != null && !logo.isEmpty()
-        && playerCount >= 11;
-}
-        
-    
-       @Override
-    public String toString() {
-        return "Club{" +
-                "name='" + name + '\'' +
-                ", code='" + code + '\'' +
-                ", country='" + country + '\'' +
-                ", stadium='" + stadiumName + '\'' +
-                ", players=" + playerCount +
-                '}';
+    public boolean isValid() {
+        if (name == null || name.isEmpty()
+                || code == null || code.isEmpty()
+                || country == null || country.isEmpty()
+                || foundedYear <= 1800
+                || stadiumName == null || stadiumName.isEmpty()
+                || logo == null || logo.isEmpty()) {
+            throw new IllegalStateException("Club information is incomplete.");
+        }
+
+        if (players == null || playerCount == 0) {
+            throw new IllegalStateException("Club has no players.");
+        }
+
+        if (playerCount < 16) {
+            throw new IllegalStateException("Club must have at least 16 players.");
+        }
+
+        int goalkeepers = 0;
+        boolean hasPositions = false;
+
+        for (int i = 0; i < playerCount; i++) {
+            IPlayer p = players[i];
+            if (p != null && p.getPosition() != null) {
+                hasPositions = true;
+
+                String posDesc = p.getPosition().getDescription().toLowerCase();
+                if (posDesc.contains("goalkeeper") || posDesc.contains("gk")) {
+                    goalkeepers++;
+                }
+            }
+        }
+
+        if (!hasPositions) {
+            throw new IllegalStateException("Club has no players with valid positions.");
+        }
+
+        if (goalkeepers == 0) {
+            throw new IllegalStateException("Club must have at least 1 goalkeeper.");
+        }
+
+        return true;
     }
-   
+
+    /**
+     * Returns a string representation of the club.
+     *
+     * @return String with club summary
+     */
+    @Override
+    public String toString() {
+        return "Club{"
+                + "name='" + name + '\''
+                + ", code='" + code + '\''
+                + ", country='" + country + '\''
+                + ", stadium='" + stadiumName + '\''
+                + ", players=" + playerCount
+                + '}';
+    }
+
+    /**
+     * Exports the club data to a JSON file.
+     *
+     * @throws IOException If file writing fails
+     * @throws UnsupportedOperationException If method is not implemented
+     */
     @Override
     public void exportToJson() throws IOException {
-        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+        throw new UnsupportedOperationException("Not supported yet.");
     }
 }
-
